bfab873c68cc57e03c718769c61b980ec07bb675
Vleuten,Yannick Y.P. van der <y.vandervleuten@student.fontys.nl>
2017-09-27 10:34:59 +0200

    Eerste SQL Query

---
+++ Bratwurst/Content/SQLDataLayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Bratwurst.Classes;
+
+namespace Bratwurst.Content
+{
+    public class SQLDataLayer
+    {
+        private MySqlConnection connection;
+
+        public SQLDataLayer()
+        {
+            Database db = new Database();
+            this.connection = db.getConnection();
+        }
+
+        private List<Photo> getPictures()
+        {
+            List<Photo> photos = new List<Photo>();
+            using (connection)
+            {
+                string queryString = "SELECT *, (SELECT COUNT(*) FROM vote vo2, photo ph2 WHERE vo2.photoid = ph2.id AND ph1.id = ph2.id) AS amount FROM photo ph1, vote vo1 WHERE ph1.id = vo1.photoid";
+                MySqlCommand cmd = new MySqlCommand(queryString, connection);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    int photoid = rdr.GetInt32("id");
+                    string caption = rdr.GetString("caption");
+                    string image = rdr.GetString("imagedata");
+                    string story = rdr.GetString("story");
+                    string tags = rdr.GetString("tags");
+                    string credit = rdr.GetString("credit");
+                    int amountOfVoters = rdr.GetInt32("amount");
+                    photos.Add(new Photo(photoid, caption, image, story, tags, credit, amountOfVoters));
+                }
+
+            }
+                return photos;
+        }
+    }
+}
