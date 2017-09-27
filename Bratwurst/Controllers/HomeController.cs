6078142bb774ecf9df85ab5fc58ba5a25c7bc3ee
Berghs,Sander S.C.R.H. <s.berghs@student.fontys.nl>
2017-09-27 10:48:20 +0200

    PA PA KA KA KA

--- Bratwurst/Controllers/HomeController.cs
+++ Bratwurst/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using Bratwurst.Classes;
-using Bratwurst.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Bratwurst.Controllers
 {
