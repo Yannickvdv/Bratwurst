90ede84344e6d6985bd6c805a3a20c9022c71a70
Berghs,Sander S.C.R.H. <s.berghs@student.fontys.nl>
2017-09-27 11:14:13 +0200

    Commit Sudoku

--- Bratwurst/Controllers/HomeController.cs
+++ Bratwurst/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using Bratwurst.Content;
+using Bratwurst.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace Bratwurst.Controllers
 {
     public class HomeController : Controller
     {
+        SQLDataLayer sql = new SQLDataLayer();
 
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var model = new List<Photo>();
+
+            foreach(Photo p in sql.getPictures())
+            {
+                model.Add(p);
+            }
+
+            return View(model);
         }
     }
 }
